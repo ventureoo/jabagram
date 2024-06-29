ventureoo/jabagram

commit ea8f68e9206df3e5080bca62eacf97eeaa5b0579

jabagram/xmpp: Do not load message history when reconnecting

jabagram/xmpp.py
@@ -86,7 +86,7 @@ class XmppClient(ClientXMPP, ChatHandlerFactory):
     async def start(self):
         self.connect()
 
-    def create_handler(
+    async def create_handler(
             self,
             address: str,
             muc: str,
@@ -96,7 +96,11 @@ class XmppClient(ClientXMPP, ChatHandlerFactory):
         self.add_event_handler(
             f"muc::{muc}::got_online", handler.nick_change_handler
         )
-        self.plugin['xep_0045'].join_muc(JID(muc), BRIDGE_DEFAULT_NAME)
+        await self.plugin['xep_0045'].join_muc_wait(
+            JID(muc),
+            BRIDGE_DEFAULT_NAME,
+            maxstanzas=0
+        )
         self.__dispatcher.add_handler(address, handler)
 
 

jabagram/model.py
@@ -88,7 +88,7 @@ class ChatHandler(ABC):
 
 class ChatHandlerFactory(ABC):
     @abstractmethod
-    def create_handler(
+    async def create_handler(
         self,
         address: str,
         muc: str,

jabagram/telegram.py
@@ -294,7 +294,7 @@ class TelegramClient(ChatHandlerFactory):
         )
         self.__service.register_factory(self)
 
-    def create_handler(
+    async def create_handler(
         self,
         address: str,
         muc: str,

commit 114a45fd1e2c8b26df0ba076eaa2f58f7a947b5c

Wrap chat loading in a coroutine
Fixes: ea8f68e9206df3e5080bca62eacf97eeaa5b0579

jabagram/database.py
@@ -19,7 +19,7 @@
 import logging
 import sqlite3
 from sqlite3 import Error, connect
-from typing import Callable, Dict, List
+from typing import Dict, List
 
 from .cache import Cache
 from .model import ChatHandlerFactory
@@ -59,16 +59,18 @@ class Database():
         except Error as e:
             self.__logger.error("Can not add chats pair: %s", e)
 
-    def load(self, callback: Callable) -> None:
+    def get_chats(self) -> List:
+        res = []
         try:
             with sqlite3.connect(self.__path) as con:
                 cursor = con.cursor()
                 for row in cursor.execute("SELECT telegram_id,muc FROM chats"):
-                    telegram, muc = row
-                    callback(str(telegram), muc)
+                    res.append(row)
 
         except Error as e:
             self.__logger.error("Can't add chats pair: %s", e)
+
+        return res
 
     def remove(self, chat: str):
         try:
@@ -92,7 +94,7 @@ class ChatService():
         self.__key = key
         self.__logger = logging.getLogger(__class__.__name__)
 
-    def bind(self, muc: str, key: str) -> None:
+    async def bind(self, muc: str, key: str) -> None:
         telegram_id: str | None = self.__pending_chats.get(muc)
 
         if telegram_id is None:
@@ -105,23 +107,27 @@ class ChatService():
         self.__logger.info('New chat pair binded: %s - %s', muc, telegram_id)
         self.__database.add(telegram_id, muc)
         del self.__pending_chats[muc]
-        self.__spawn_handlers(telegram_id, muc)
+        await self.__spawn_handlers(telegram_id, muc)
 
-    def __spawn_handlers(self, telegram_id, muc):
+    async def __spawn_handlers(self, telegram_id, muc):
         cache = Cache(100)
         self.__logger.info(
             "Create handlers for chat %s and MUC: %s", telegram_id, muc
         )
         # Notify all factories to create chat message handlers
         for factory in self.__factories:
-            factory.create_handler(telegram_id, muc, cache)
+            await factory.create_handler(telegram_id, muc, cache)
 
     def register_factory(self, factory: ChatHandlerFactory) -> None:
         self.__factories.append(factory)
 
-    def load_chats(self) -> None:
+    async def load_chats(self) -> None:
         self.__logger.info("Loading chats from database...")
-        self.__database.load(self.__spawn_handlers)
+        chats = self.__database.get_chats()
+
+        for chat in chats:
+            telegram_id, muc = chat
+            await self.__spawn_handlers(str(telegram_id), muc)
 
     def pending(self, muc: str, chat: str) -> None:
         self.__logger.info(

jabagram/xmpp.py
@@ -118,15 +118,15 @@ class XmppClient(ClientXMPP, ChatHandlerFactory):
         self.send_presence()
 
         if not self.__reconnecting:
-            self.__service.load_chats()
+            await self.__service.load_chats()
 
     async def __on_connected(self, _):
         self.__logger.info("Successfully connected.")
 
-    def __invite_callback(self, invite):
+    async def __invite_callback(self, invite):
         muc = str(invite['groupchat_invite']['jid'])
         key = invite['groupchat_invite']['reason']
-        self.__service.bind(muc, key)
+        await self.__service.bind(muc, key)
 
     async def __process_message(self, message):
         sender = message['mucnick']